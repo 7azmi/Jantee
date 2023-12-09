d4b1c8eb657a63b7f6705c11a0ab51b4803caa53
7azmi <a7.h3.04@gmail.com>
2023-12-09 21:47:30 +0800

    Database.py added

--- database/database.py
+++ database/database.py
@@ -1,148 +1,117 @@
-import json
 import os
-from typing import Dict
-
 import psycopg2
+from datetime import date
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv("app/.env")
 
-db_host = os.environ.get("PGHOST")
-db_user = os.environ.get("PGUSER")
-db_password = os.environ.get("PGPASSWORD")
-db_name = os.environ.get("PGDATABASE")
+# Database credentials
+db_host = os.environ.get("PGHOST", "your_host")
+db_user = os.environ.get("PGUSER", "your_user")
+db_password = os.environ.get("PGPASSWORD", "your_password")
+db_name = os.environ.get("PGDATABASE", "your_database")
+
+# Database table names
+USERS_TABLE = 'users'
+GROUPS_TABLE = 'groups'
+DAILY_PUSHUP_RECORD_TABLE = 'daily_pushup_record'
+
+GROUP_ID_COLUMN = 'group_id'
+PUSHUP_GOAL_COLUMN = 'pushup_goal'
+PUNISHMENT_COLUMN = 'punishment'
+
+USER_TELEGRAM_ID_COLUMN = 'user_telegram_id'
+DATE_COLUMN = 'date'
+PUSHUPS_COUNT_COLUMN = 'pushups_count'
 
 def create_db():
-    # Create a connection to the database
-    with psycopg2.connect(
-        host=db_host, user=db_user, password=db_password, database=db_name
-    ) as conn:
+    with psycopg2.connect(host=db_host, user=db_user, password=db_password, database=db_name) as conn:
         c = conn.cursor()
-        c.execute(
-            """
-            CREATE TABLE IF NOT EXISTS users (
-                telegram_id TEXT PRIMARY KEY,
-                enquiries INTEGER,
-                current_language TEXT
+        # Create Users Table
+        c.execute(f"""
+            CREATE TABLE IF NOT EXISTS {USERS_TABLE} (
+                telegram_id INTEGER PRIMARY KEY,
+                group_id INTEGER,
+                FOREIGN KEY (group_id) REFERENCES {GROUPS_TABLE} (group_id)
             );
-             """
-        )
+        """)
+        # Create Groups Table
+        c.execute(f"""
+            CREATE TABLE IF NOT EXISTS {GROUPS_TABLE} (
+                group_id SERIAL PRIMARY KEY,
+                pushup_goal INTEGER,
+                punishment TEXT
+            );
+        """)
+        # Create Daily Pushup Record Table
+        c.execute(f"""
+            CREATE TABLE IF NOT EXISTS {DAILY_PUSHUP_RECORD_TABLE} (
+                user_telegram_id INTEGER,
+                date DATE,
+                pushups_count INTEGER,
+                PRIMARY KEY (user_telegram_id, date),
+                FOREIGN KEY (user_telegram_id) REFERENCES {USERS_TABLE} (telegram_id)
+            );
+        """)
         conn.commit()
 
-def delete_db():
-    # Create a connection to the database
-    with psycopg2.connect(
-        host=db_host, user=db_user, password=db_password, database=db_name
-    ) as conn:
+def set_data(table, primary_key, column, new_data, key_value):
+    with psycopg2.connect(host=db_host, user=db_user, password=db_password, database=db_name) as conn:
         c = conn.cursor()
-        c.execute("DROP TABLE IF EXISTS users;")
+        query = f"UPDATE {table} SET {column} = %s WHERE {primary_key} = %s;"
+        c.execute(query, (new_data, key_value))
         conn.commit()
 
-def add_new_user(user: str, language: str = "en"):
-    new_user = {"telegram_id": user, "enquiries": 0, "current_language": language}
-
-    with psycopg2.connect(
-        host=db_host, user=db_user, password=db_password, database=db_name
-    ) as conn:
+def get_data(table, primary_key, column, key_value):
+    with psycopg2.connect(host=db_host, user=db_user, password=db_password, database=db_name) as conn:
         c = conn.cursor()
-        c.execute(
-            "INSERT INTO users (telegram_id, enquiries, current_language) VALUES (%s, %s, %s) ON CONFLICT DO NOTHING;",
-            (new_user["telegram_id"], new_user["enquiries"], new_user["current_language"]),
-        )
+        query = f"SELECT {column} FROM {table} WHERE {primary_key} = %s;"
+        c.execute(query, (key_value,))
+        result = c.fetchone()
+        return result[0] if result else None
+
+def add_daily_pushup_record(user_id, pushups_count):
+    with psycopg2.connect(host=db_host, user=db_user, password=db_password, database=db_name) as conn:
+        c = conn.cursor()
+        c.execute(f"""
+            INSERT INTO {DAILY_PUSHUP_RECORD_TABLE} (user_telegram_id, date, pushups_count)
+            VALUES (%s, %s, %s)
+            ON CONFLICT (user_telegram_id, date) DO UPDATE SET pushups_count = {DAILY_PUSHUP_RECORD_TABLE}.pushups_count + EXCLUDED.pushups_count;
+        """, (user_id, date.today(), pushups_count))
         conn.commit()
 
+def add_user(telegram_id, group_id):
+    with psycopg2.connect(host=db_host, user=db_user, password=db_password, database=db_name) as conn:
+        c = conn.cursor()
+        c.execute(f"""
+            INSERT INTO {USERS_TABLE} (telegram_id, group_id)
+            VALUES (%s, %s) ON CONFLICT (telegram_id) DO NOTHING;
+        """, (telegram_id, group_id))
+        conn.commit()
 
-def update_enquiry_user(telegram_id):
-    try:
-        # Create a connection to the database
-        with psycopg2.connect(
-                host=db_host, user=db_user, password=db_password, database=db_name
-        ) as conn:
-            c = conn.cursor()
+def add_group(pushup_goal, punishment):
+    with psycopg2.connect(host=db_host, user=db_user, password=db_password, database=db_name) as conn:
+        c = conn.cursor()
+        c.execute(f"""
+            INSERT INTO {GROUPS_TABLE} (pushup_goal, punishment)
+            VALUES (%s, %s) RETURNING group_id;
+        """, (pushup_goal, punishment))
+        group_id = c.fetchone()[0]
+        conn.commit()
+        return group_id
 
-            # Increment the user's enquiries by 1
-            c.execute(
-                """
-                UPDATE users
-                SET enquiries = enquiries + 1
-                WHERE telegram_id = %s
-                """,
-                (telegram_id,)
-            )
-            conn.commit()
-
-    except psycopg2.Error as e:
-        print(f"Error updating user: {e}")
-
-
-def set_user_language(telegram_id, current_language):
-    try:
-        # Create a connection to the database
-        with psycopg2.connect(
-                host=db_host, user=db_user, password=db_password, database=db_name
-        ) as conn:
-            c = conn.cursor()
-
-            # Update the user's current_language
-            c.execute(
-                """
-                UPDATE users
-                SET current_language = %s
-                WHERE telegram_id = %s
-                """,
-                (current_language, telegram_id)
-            )
-            conn.commit()
-
-    except psycopg2.Error as e:
-        print(f"Error setting user language: {e}")
-
-
-def get_user_language(telegram_id):
-    try:
-        # Create a connection to the database
-        with psycopg2.connect(
-                host=db_host, user=db_user, password=db_password, database=db_name
-        ) as conn:
-            c = conn.cursor()
-
-            # Retrieve the user's current_language
-            c.execute(
-                """
-                SELECT current_language
-                FROM users
-                WHERE telegram_id = %s
-                """,
-                (telegram_id,)
-            )
-            result = c.fetchone()
-
-            if result:
-                return result[0]  # Return the current_language
-            else:
-                return None  # User not found
-
-    except psycopg2.Error as e:
-        print(f"Error getting user language: {e}")
+def get_users_in_group(group_id):
+    with psycopg2.connect(host=db_host, user=db_user, password=db_password, database=db_name) as conn:
+        c = conn.cursor()
+        c.execute(f"SELECT telegram_id FROM {USERS_TABLE} WHERE group_id = %s;", (group_id,))
+        return [row[0] for row in c.fetchall()]
 
 if __name__ == "__main__":
-    #delete_db()
-
     create_db()
-    user = "323232"
-    #add_new_user(user)
-
-    #question = "What's the meaning of life?"
-    #answer = "42"
-
-    #update_enquiry_user(user)
-
-    set_user_language(user, 'ar')
-    print(get_user_language(user))
-    #print("after update: ", row)
-
-    #reset_history_user(user)
-    #print("\n" * 4)
-
-    #row = retrieve_history(user)
-    #print("After reset: ", row)
+    # Example usage
+    # group_id = add_group(50, "5-minute plank")
+    # add_user(123456789, group_id)
+    # add_daily_pushup_record(123456789, 30)
+    # print(get_data(USERS_TABLE, 'telegram_id', 'group_id', 123456789))
+    # print(get_users_in_group(group_id))
