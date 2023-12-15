d05a3841e91760c91ff493052fc7c94ba36495b3
7azmi <a7.h3.04@gmail.com>
2023-12-15 17:27:35 +0800

    database IDs changed to BIGINT (int64)

--- database/database.py
+++ database/database.py
@@ -20,7 +20,7 @@ GROUPS_TABLE = 'groups'
 DAILY_PUSHUP_RECORD_TABLE = 'daily_pushup_record'
 
 
-def create_db(): # not tested
+def create_db():  # not tested
     with psycopg2.connect(host=db_host, user=db_user, password=db_password, database=db_name) as conn:
         c = conn.cursor()
         # Create Groups Table with an additional 'timezone' field and telegram_id as PK
@@ -51,6 +51,28 @@ def create_db(): # not tested
             );
         """)
         conn.commit()
+
+
+def generate_database_design():
+    with psycopg2.connect(host=db_host, user=db_user, password=db_password, database=db_name) as conn:
+        c = conn.cursor()
+        c.execute("""
+            SELECT table_name, column_name, data_type 
+            FROM information_schema.columns 
+            WHERE table_schema = 'public'
+            ORDER BY table_name, ordinal_position;
+        """)
+
+        current_table = ''
+        for row in c.fetchall():
+            table_name, column_name, data_type = row
+            if table_name != current_table:
+                if current_table:
+                    print('}')
+                print(f'Table: {table_name} {{')
+                current_table = table_name
+            print(f'    {column_name} ({data_type})')
+        print('}')
 
 
 def create_connection():
@@ -113,16 +135,18 @@ def get_value(table_name, columns, condition):
         conn.close()
 
 
-def add_new_group(group_id, pushup_goal, punishment, timezone):
+def add_new_group(group_id, pushup_goal, punishment, timezone, group_name):
     table_name = GROUPS_TABLE
     data = {
-        'group_id': group_id,  # Manually specified group_id
+        'group_id': group_id,
         'pushup_goal': pushup_goal,
         'punishment': punishment,
-        'timezone': timezone  # Include timezone information
+        'timezone': timezone,
+        'group_name': group_name
     }
 
     create_row(table_name, data)
+
 
 
 def change_group_timezone(group_id, new_timezone):
@@ -145,6 +169,7 @@ def get_timezone_automatically():
     except Exception as e:
         print(f"Error while getting local timezone: {e}")
         return None
+
 
 def add_new_user(telegram_id, group_id):
     table_name = USERS_TABLE
@@ -169,13 +194,16 @@ def assign_user_to_group(telegram_id, group_id):
 def remove_user_from_group(telegram_id):
     assign_user_to_group(telegram_id, 0)
 
+
 def update_pushups(telegram_id, pushups_count_increase):
     try:
         conn = create_connection()
         c = conn.cursor()
 
         # Fetch group_id and timezone offset for the user
-        c.execute(f"SELECT g.timezone FROM {USERS_TABLE} u JOIN {GROUPS_TABLE} g ON u.group_id = g.group_id WHERE u.telegram_id = %s;", (telegram_id,))
+        c.execute(
+            f"SELECT g.timezone FROM {USERS_TABLE} u JOIN {GROUPS_TABLE} g ON u.group_id = g.group_id WHERE u.telegram_id = %s;",
+            (telegram_id,))
         timezone_offset = c.fetchone()
 
         if timezone_offset:
@@ -216,17 +244,99 @@ def update_pushups(telegram_id, pushups_count_increase):
         conn.close()
 
 
-#create_db()
-#add_new_group(0, 100, 'A virtual group for non-group users')  # Manually specify group_id as 1
-#add_new_group(123, 100, 'Fake group')
-#assign_user_to_group(123456789, 123)
-#create_db()
-change_group_timezone(321, '-07')
-#add_new_group(321, 50, "nothing", get_timezone_automatically())
-#print(get_timezone_automatically())
-#update_pushups(123456789, 9)
+# for testing
+def create_pushups(telegram_id, pushups_count_increase, timezone_offset):
+    try:
+        conn = create_connection()
+        c = conn.cursor()
+
+        if timezone_offset:
+            offset_hours = int(timezone_offset)
+            current_utc_time = datetime.utcnow()
+            current_local_time = current_utc_time + timedelta(hours=offset_hours)
+            current_date = current_local_time.date().isoformat()
+        else:
+            print("User's group or timezone not found.")
+            return
+
+        # Check if a record exists for the current date
+        c.execute(f"""
+            SELECT pushups_count FROM {DAILY_PUSHUP_RECORD_TABLE}
+            WHERE user_telegram_id = %s AND date = %s;
+            """, (telegram_id, current_date))
+        existing_record = c.fetchone()
+
+        if existing_record:
+            # Update the existing record
+            new_pushups_count = existing_record[0] + pushups_count_increase
+            c.execute(f"""
+                UPDATE {DAILY_PUSHUP_RECORD_TABLE}
+                SET pushups_count = %s
+                WHERE user_telegram_id = %s AND date = %s;
+                """, (new_pushups_count, telegram_id, current_date))
+        else:
+            # Insert a new record
+            c.execute(f"""
+                INSERT INTO {DAILY_PUSHUP_RECORD_TABLE} (user_telegram_id, date, pushups_count)
+                VALUES (%s, %s, %s);
+                """, (telegram_id, current_date, pushups_count_increase))
+
+        conn.commit()
+    except psycopg2.Error as e:
+        print(f"Database error: {e}")
+    finally:
+        conn.close()
+
+
+def upgrade_id_columns_to_bigint(db_host, db_user, db_password, db_name, GROUPS_TABLE, USERS_TABLE,
+                                 DAILY_PUSHUP_RECORD_TABLE):
+    with psycopg2.connect(host=db_host, user=db_user, password=db_password, database=db_name) as conn:
+        c = conn.cursor()
+
+        # Alter group_id in GROUPS_TABLE to BIGINT
+        sql_command = f"ALTER TABLE {GROUPS_TABLE} ALTER COLUMN group_id TYPE BIGINT USING group_id::BIGINT;"
+        print("Executing SQL command:", sql_command)
+        c.execute(sql_command)
+
+        # Alter telegram_id in USERS_TABLE to BIGINT
+        sql_command = f"ALTER TABLE {USERS_TABLE} ALTER COLUMN telegram_id TYPE BIGINT USING telegram_id::BIGINT;"
+        print("Executing SQL command:", sql_command)
+        c.execute(sql_command)
+
+        # Alter group_id in USERS_TABLE to BIGINT (Foreign Key)
+        sql_command = f"ALTER TABLE {USERS_TABLE} ALTER COLUMN group_id TYPE BIGINT USING group_id::BIGINT;"
+        print("Executing SQL command:", sql_command)
+        c.execute(sql_command)
+
+        # Alter user_telegram_id in DAILY_PUSHUP_RECORD_TABLE to BIGINT
+        sql_command = f"ALTER TABLE {DAILY_PUSHUP_RECORD_TABLE} ALTER COLUMN user_telegram_id TYPE BIGINT USING user_telegram_id::BIGINT;"
+        print("Executing SQL command:", sql_command)
+        c.execute(sql_command)
+
+        conn.commit()
+
+
+# upgrade_id_columns_to_bigint(db_host, db_user, db_password, db_name, GROUPS_TABLE, USERS_TABLE, DAILY_PUSHUP_RECORD_TABLE)
+generate_database_design()
+#add_new_group(-1002127852426, 100 ,'Pay RM10', '+08', '100 Pushups or -RM10')
+# create_db()
+# add_new_group(0, 100, 'A virtual group for non-group users')  # Manually specify group_id as 1
+# add_new_group(123, 100, 'Fake group')
+# assign_user_to_group(123456789, 123)
+# create_db()
+# add_new_group(-1002127852426, 100, "Pay RM10", get_timezone_automatically())
+# change_group_timezone(321, get_timezone_automatically())
+# create_pushups(2012089704, 50, -20)
+# add_new_group('-1002127852426')
+
+# Sadman 2012089704
+# update_pushups(2012089704, 20)
+# change_group_timezone(321, '-07')
+# add_new_group(321, 50, "nothing", get_timezone_automatically())
+# print(get_timezone_automatically())
+# update_pushups(123456789, 9)
 # Manually specify group_id as 1
-#add_new_user(123456789, 0)
+# add_new_user(123456789, 0)
 # delete_db()
 # Example usage
 # group_id = add_group(123, 50, "RM5 donation")
