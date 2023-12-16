0c2147e997c76f7fe554362bf216884533e64364
7azmi <a7.h3.04@gmail.com>
2023-12-17 04:26:44 +0800

    conversations.json Data structure added

---
+++ app/data/data.py
@@ -0,0 +1,29 @@
+import json
+import random
+
+
+# Load the messages from a JSON file
+def load_messages(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+# Example function to get a message
+def get_message(messages, language, situation, order='random'):
+    """
+    Retrieves a message for a given language and situation.
+    By default, it picks randomly unless specified to use 'sequential' order.
+    """
+    language_messages = messages.get(language, {})
+    situation_messages = language_messages.get(situation, ["Oops, I don't have a message for this situation yet!"])
+
+    if order == 'random':
+        return random.choice(situation_messages)
+    elif order == 'sequential':
+        # Implement sequential logic with state tracking as needed
+        return situation_messages[0]  # Placeholder for the first message.
+
+
+# Example usage
+messages = load_messages('app/data/conversations.json')
+print(get_message(messages, 'en', 'start_new_user'))  # Random English message for a new user
