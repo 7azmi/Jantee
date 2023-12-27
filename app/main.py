6396cd7f8ef9f9164b1d961104383db91921c1bb
7azmi <a7.h3.04@gmail.com>
2023-12-27 21:11:22 +0800

    dotenv defined

--- app/main.py
+++ app/main.py
@@ -6,7 +6,9 @@ from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app.handlers import *
+from dotenv import load_dotenv
 
+# Load environment variables from a .env file
 load_dotenv("app/.env")
 
 
