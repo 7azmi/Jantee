35c28e4bd0b666792c3b714c89bb756512f5df41
7azmi <a7.h3.04@gmail.com>
2023-12-30 02:48:47 +0800

    streak added and tested

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ import os
 import sys
 from telegram.ext import Filters, MessageHandler, Updater, ChatMemberHandler
 
-import app.database.database
 from database import API
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
