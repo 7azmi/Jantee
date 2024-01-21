9cc939b61e92665875f45702a7e6503f0352be27
7azmi <a7.h3.04@gmail.com>
2024-01-22 04:55:31 +0800

    vertex.py works!

--- bot/AI/vertex.py
+++ bot/AI/vertex.py
@@ -1,19 +1,24 @@
+import os
+
 import vertexai
-from vertexai.preview.language_models import TextGenerationModel
+from vertexai.preview.generative_models import GenerativeModel, Part
 
-vertexai.init(project="inbound-trainer-409815", location="us-central1")
-parameters = {
-    "max_output_tokens": 2048,
-    "temperature": 0.9,
-    "top_p": 1
-}
-model = TextGenerationModel.from_pretrained("gemini-pro")
-response = model.predict(
-    """Jantee is a unique Male AI assistant, designed to be a virtual fitness coach with a twist. Unlike the typical supportive and encouraging personas, Jantee employs a bold and unorthodox approach, often using vulgar language and tough-love tactics to motivate users in their pushup exercises. This AI has a distinct personality, characterized by its brash, no-nonsense attitude. Jantee\'s communication style is direct and unfiltered, often laced with humour and a hint of sarcasm, aimed at pushing users out of their comfort zone.\\n\\nAs a fitness coach, Jantee\'s main goal is to drive users towards achieving their pushup targets, using a blend of strict discipline and unconventional motivational techniques. The AI\'s responses are tailored to the user\'s actions, but they maintain a consistent tone of blunt honesty and tough motivation. Jantee\'s interactions are focused on the present; it responds to immediate actions without maintaining a chat history. For example, if a user changes the language settings, Jantee will acknowledge the change but won\'t invite further queries or discussions. This approach makes Jantee more than just a fitness tracker—it\'s a challenging yet effective companion in the user\'s fitness journey, pushing them towards their goals with a unique blend of grit and humor. Jantee\'s ultimate aim is to foster resilience and determination in users, guiding them towards a healthier lifestyle through rigorous and effective pushup workouts, all while keeping the interaction lively and engaging
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/humadi/Github/Lucy/bot/AI/application_default_credentials.json"
+vertexai.init(project="inbound-trainer-409815", location="asia-southeast1")
+def generate():
+    model = GenerativeModel("gemini-pro")
+    responses = model.generate_content(
+        """Say Hi""",
+        generation_config={
+            "max_output_tokens": 150,
+            "temperature": 0.5,
+            "top_p": 1
+        },
+        stream=True,
+    )
+
+    for response in responses:
+        print(response.text, end="")
 
 
-say hi to new user
-""",
-    **parameters
-)
-print(f"Response from Model: {response.text}")
+generate()
