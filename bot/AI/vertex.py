6cea918d5d200cbae5ba0251cbdb6980a8024712
7azmi <a7.h3.04@gmail.com>
2024-01-24 07:40:49 +0800

    .

--- bot/AI/vertex.py
+++ bot/AI/vertex.py
@@ -5,7 +5,7 @@ from vertexai.preview.language_models import TextGenerationModel
 import vertexai
 from vertexai.preview.language_models import TextGenerationModel
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/humadi/Github/Lucy/bot/AI/application_default_credentials.json"
+#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/humadi/Github/Lucy/bot/AI/application_default_credentials.json"
 
 #vertexai.init(project="inbound-trainer-409815", location="asia-southeast1")
 
@@ -15,7 +15,7 @@ parameters = {
     "temperature": 0.5,
     "top_p": 1
 }
-model = TextGenerationModel.from_pretrained("gemini-pro")
+model = TextGenerationModel("gemini-pro")
 response = model.predict(
     """Jantee is a male AI fitness coach with a bold, unorthodox style. Unlike typical supportive AI, he uses tough-love and vulgar language for motivation. Known for his brash, no-nonsense attitude, Jantee\'s communication is direct, humorous, and slightly sarcastic. He focuses on pushup exercises, driving users with strict discipline and unconventional methods. Jantee tailors responses to immediate actions, avoiding extended chat histories. His approach is bluntly honest, aiming to build resilience and determination in users, making their fitness journey rigorous yet engaging with a unique blend of grit and humor.
 
@@ -25,7 +25,7 @@ chat_response:
 """,
     **parameters
 )
-#print(f"Response from Model: {response.text}")
+print(f"Response from Model: {response.text}")
 # async def execute_in_parallel(n):
 #     with ThreadPoolExecutor() as executor:
 #         loop = asyncio.get_event_loop()
